454db62b30dc2ebb69a1f8a3817f82452c6d1d54
luis Miguel <luism.vieira@iclou.comgit initexitrexitgit config --global user.email luism.vieira@icloud.com>
2023-10-05 11:44:03 -0300

    pasta python

---
+++ rename.py
@@ -0,0 +1,9 @@
+
+saldo = float(2000.0)
+saque = float(input("Qual o valor do saque? "))
+
+if saldo >= saque:
+    print("saque realizado")
+    
+if saldo < saque:
+    print("saldo insuficiente")
